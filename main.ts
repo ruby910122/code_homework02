41764ae16382ea89cc17ec48cf262c87b52912c0
ruby910122 <71751285+ruby910122@users.noreply.github.com>
2020-10-28 14:12:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@ basic.forever(function () {
     for (let I = 0; I <= 8; I++) {
         for (let J = 0; J <= 4; J++) {
             led.plot(4 - J, 4 - (I - J))
-            basic.pause(100)
         }
+        basic.pause(200)
     }
     basic.clearScreen()
     for (let I = 0; I <= 8; I++) {
         for (let J = 0; J <= 4; J++) {
             led.plot(J, I - J)
         }
-        basic.pause(100)
+        basic.pause(200)
     }
     basic.clearScreen()
 })
